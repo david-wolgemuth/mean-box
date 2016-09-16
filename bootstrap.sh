78e1ebb2a521cf5af3f523d784b7d2eef7e989cf
David Wolgemuth <davidjwolgemuth@gmail.com>
2016-09-15 18:19:30 -0700

    Cleaned printing in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,26 @@
 
-echo "\n\n\nUpdating Package Information\n\n\n"
+function print_description { printf "\n\n\n$1\n\n\n\n" }
+
+print_description "Updating Package Information"
 apt-get -y update
 
-echo "\n\n\nInstalling Git\n\n\n"
+print_description "Installing Git"
 apt-get -y install git
 
-echo "\n\n\nInstalling Dependencies\n\n\n"
+print_description "Installing Dependencies"
 apt-get -y install -y build-essential openssl libssl-dev pkg-config
 
-echo "\n\n\nInstalling Node\n\n\n"
+print_description "Installing Node"
 apt-get -y install node
 apt-get -y install npm
 
-echo "\n\n\nInstalling Mongo\n\n\n"
+print_description "Installing Mongo"
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt-get update
 apt-get install -y mongodb-org
 
-echo "\n\n\nUpdating Node\n\n\n"
+print_description "Updating Node"
 npm cache clean -f
 npm install -g n
 n stable
